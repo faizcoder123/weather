049613068b1ef2bb749cd31487393a1b417131c5
faizcoder123 <faizhaq241@gmail.com>
2020-10-24 18:11:29 +0530

    update

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
-const form         = document.querySelector('form')
+const form        = document.querySelector('form')
 const search      = document.querySelector('input')
 const messageOne  = document.querySelector('#message-1')
-const messageTwo  = document.querySelector('#message-2')
+
 
 form.addEventListener('submit', (e) => {
 //      console.log("inside")
@@ -10,15 +10,16 @@ form.addEventListener('submit', (e) => {
     const location = search.value
 
     messageOne.textContent = 'Loading....'
-    messageTwo.textContent = ''  //clear last qury from page first..
+  //clear last qury from page first..
     // to fetch jason data and display it
     fetch('/weather?address=' + location).then((response) => {//fetch from this api and run promise
         response.json().then((data) => { // use json data and display
             if (data.error) {
                 messageOne.textContent = data.error
-            } else {
-                messageOne.textContent = data.address
-                messageTwo.textContent = data.forecast
+            }
+            else {
+                messageOne.textContent = data.forecast
+              
             }
         })
     })
